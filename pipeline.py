8ae82ec9713d5709b06641425f6e4687bf4ccf75
Mattias Dürrmeier <matt.duerr@pm.me>
2024-11-12 17:53:21 +0100

    Add statsmodels with a scikit-learn wrapper

--- pipeline.py
+++ pipeline.py
@@ -4,31 +4,57 @@ from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
 
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+
 import numpy as np
 import numpy.typing as npt
 from model import Model
 from typing import Callable
 
-# Model to find:
-# - HW (Holt-Winter) Additive
-# - HW (Holt-Winter) Multiplicative
-# - AutoEnsembler: are these variations of XGBoost?
 def create_pipelines() -> list[Model]:
-    # Stat Models
-    # TODO: add them
-
     # Zero Model
     zm = ZeroModel()
+
+    # Stat Models
+    # bats = None # TODO: what to use for BATS?
+    arima = SMWrapper(ARIMA)
+    hw_add = SMWrapper(ExponentialSmoothing, **{"seasonal": "add", "seasonal_periods": 4})
+    hw_mult = SMWrapper(ExponentialSmoothing, **{"seasonal": "Multiplicative", "seasonal_periods": 4})
 
     # ML Models
     svr = SVR()
     rfr = RandomForestRegressor()
     xgb = XGBRegressor()
+    # AutoEnsembler: are these variations of XGBoost?
 
-    # Transformers
+    # Transformers: do we use it?
     log_transformer = FunctionTransformer(np.log, validate=True)
 
-    return [zm, svr, rfr, xgb]
+    return [zm, arima, svr, rfr, xgb]
+
+
+class SMWrapper(Model):
+    """
+    A Wrapper for statsmodels regressor with true scikit-learn API.
+    Required because statsmodel needs the data to initialize the model, but we want this to happen when we call fit.
+    """
+    def __init__(self, model_class, **kwargs):
+        self.model_class = model_class
+        self.kwargs = kwargs
+
+    def fit(self, X: npt.NDArray, y: npt.NDArray) -> 'SMWrapper':
+        m = self.model_class(y, **self.kwargs)
+        self.model = m.fit()
+        return self
+
+    def predict(self, X: npt.NDArray) -> npt.NDArray:
+        return self.model.forecast(len(X))
+
+    def score(self, X: npt.NDArray, y: npt.NDArray) -> float:
+        from sklearn.metrics import r2_score
+        y_pred = self.model.forecast(len(X))
+        return r2_score(y, y_pred)
 
 
 class ZeroModel(Model):
